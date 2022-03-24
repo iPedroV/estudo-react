60b1790755ee7ade80f4dd4f1cd78ddd331dc1f7
Pedro Vitor <pedro.vcosta405@gmail.com>
2022-03-23 22:00:04 -0300

    salvar em casa

--- src/components/Cartao.jsx
+++ src/components/Cartao.jsx
@@ -5,7 +5,7 @@ const Cartao = (props) => {
     return (
         <Card border="dark" className="mb-3" style={{ width: '15rem', height: '17rem' }}>
 
-            <Card.Img src="https://i.superesportes.com.br/81WRvS0ZqVjw5kHq0bzEibTbpcs=/smart/imgsapp.mg.superesportes.com.br/app/foto_126510467054/2022/02/12/13330/20220212210407264685a.jpg" />
+            <Card.Img src="https://i.superesportes.com.br/81WRvS0ZqVjw5kHq0bzEibTbpcs=/smart/imgsapp.mg.superesportes.com.br/app/foto_126510467054/2022/02/12/13330/20220212210407264685a.jpg" ></Card.Img>
             <Card.Body>
                 <Card.Title>{props.titulo}</Card.Title>
                 <Card.Text>
