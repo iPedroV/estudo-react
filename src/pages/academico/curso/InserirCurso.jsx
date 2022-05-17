b99250d0ca9d5b3534414cc1335fec106e611240
Pedro Vitor <pedro.vcosta405@gmail.com>
2022-05-16 22:12:07 -0300

    Apenas visualização de teste

---
+++ src/pages/academico/curso/InserirCurso.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import { Form, Button } from 'react-bootstrap'
+import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
+import { FaCheck } from 'react-icons/fa'
+import { BsArrowLeft } from 'react-icons/bs'
+
+const InserirCurso = () => {
+
+    const { register, handleSubmit, formState: { errors } } = useForm()
+
+    function salvar(dados) {
+        console.log(dados)
+        //ok
+    }
+
+    return (
+        <div>
+            <h1>Cadastrar Cursos</h1>
+            <Form>
+                <Form.Group className="mb-3" controlId="nome">
+                    <Form.Label>Nome: </Form.Label>
+                    <Form.Control type="text" {...register("nome", {
+                        required: 'Campo Obrigatório',
+                        minLength: {
+                            value: 3,
+                            message: "Qtd mínima de caracteres não informado"
+                        },
+                        maxLength: {
+                            value: 50,
+                            message: "Qtd máxima de caracteres ultrapassada"
+                        },
+                    })} />
+                    {errors.nome && <span className="text-danger">{errors.nome.message}</span>}
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="duracao">
+                    <Form.Label>Duração: </Form.Label>
+                    <Form.Control type="number" {...register("duracao", {
+
+                        minLength: {
+                            value: 1,
+                            message: "Qtd mínima de caracteres não informado"
+                        },
+                        maxLength: {
+                            value: 11,
+                            message: "Qtd máxima de caracteres ultrapassada"
+                        },
+                    })} />
+                    {errors.duracao && <span className="text-danger">{errors.duracao.message}</span>}
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="modalidade">
+                    <Form.Label>Modalidade: </Form.Label>
+                    <Form.Control type="text" {...register("modalidade", {
+                        required: 'Campo Obrigatório',
+                        maxLength: {
+                            value: 1,
+                            message: "Qtd máxima de caracteres ultrapassada"
+                        },
+                    })} />
+                    {errors.modalidade && <span className="text-danger">{errors.modalidade.message}</span>}
+                </Form.Group>
+                <div className="text-center">
+                    <Button onClick={handleSubmit(salvar)}><FaCheck /> Salvar</Button>{' '}
+                    <Link to={-1} className='btn btn-danger'><BsArrowLeft />  Voltar</Link>
+                </div>
+            </Form>
+        </div>
+    )
+}
+
+export default InserirCurso
